0cc6683eca64cf85015a62d2ead44aac17e1d935
dbaldoni <dbaldoni@student.42roma.it>
2023-04-24 23:26:45 +0200

    ver0.3

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dbaldoni <dbaldoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 19:18:37 by dbaldoni          #+#    #+#             */
-/*   Updated: 2023/04/17 20:21:36 by dbaldoni         ###   ########.fr       */
+/*   Updated: 2023/04/24 23:07:33 by dbaldoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,59 @@
 char *ft_read(int fd)
 {
 
-int byte_read;
-char *buffer;
+    int byte_read;
+    char *buffer;
 
-buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-
-
-
-byte_read = read(fd, buffer, BUFFER_SIZE);
-
-    return(buffer);    
+    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    byte_read = read(fd, buffer, BUFFER_SIZE);
+    return (buffer);
 }
 
-char	*get_next_line(int fd)
+char *print_string(int fd)
 {
 
-    ft_read(fd);
+    int len;
+    int i;
+    char *string;
+    char *short_string;
 
+    len = 0;
+    i = 0;
+
+    string = ft_read(fd);
+
+    while (string && string[len])
+    {
+        if (string[len] == '\n')
+            break;
+        len++;
+        
+    }
+    //printf("%d",len);
+    short_string = malloc(sizeof(char) * (len));
+
+    while (i <= len)
+    {
+
+        short_string[i] = string[i];
+        i++;
+    }
+
+    short_string[i] = '\0';
+    return (short_string);
+}
+
+char *get_next_line(int fd)
+{
+    return (print_string(fd));
 }
 
 int main()
 {
 
-int fd;
+    int fd;
 
     fd = open("test.txt", O_RDONLY);
-printf("%s \n", get_next_line(fd));
-
+    printf("%s", get_next_line(fd));
+   //get_next_line(fd);
 }
